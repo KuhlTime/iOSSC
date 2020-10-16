7a30f62ac39b3bc3b96a9573a22eadffa7aa9c6c
André Kuhlmann <akuhltime@gmail.com>
2020-10-16 13:41:52 +0200

    Initial Commit

---
+++ iOSSC/iOSSCApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSSCApp.swift
+//  iOSSC
+//
+//  Created by André Kuhlmann on 16.10.20.
+//
+
+import SwiftUI
+
+@main
+struct iOSSCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
