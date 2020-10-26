f4ca945b420eafd3398377a74f592409c249d581
André Kuhlmann <akuhltime@gmail.com>
2020-10-26 11:40:07 +0100

    Add Development Mode Display
    - Added a red label that gets shown when the app gets run in development mode

--- iOSSC/iOSSCApp.swift
+++ iOSSC/iOSSCApp.swift
@@ -8,19 +8,39 @@
 import SwiftUI
 import Sentry
 
+private let env: APIManager.Enviorment = .production
+
 @main
 struct iOSSCApp: App {
-    @ObservedObject var manager = APIManager(enviorment: .production)
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    @ObservedObject var manager = APIManager(enviorment: env)
     
     var body: some Scene {
         WindowGroup {
-            if (manager.isLoggedIn || manager.isRefreshing) {
-                ContentView().environmentObject(manager)
-            } else if (manager.isLoggingIn) {
-                LoadingScreen()
-            } else {
-                LoginView().environmentObject(manager)
+            ZStack {
+                if (manager.isLoggedIn || manager.isRefreshing) {
+                    ContentView().environmentObject(manager)
+                } else if (manager.isLoggingIn) {
+                    LoadingScreen()
+                } else {
+                    LoginView().environmentObject(manager)
+                }
+                
+                if (env == .development) {
+                    VStack {
+                        HStack {
+                            Text("Development Mode")
+                                .foregroundColor(.red)
+                                .font(.caption)
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            Spacer()
+                        }
+                        .padding(.horizontal, 21)
+                        .padding(.top, -4)
+                        Spacer()
+                    }
+                }
             }
         }
     }
