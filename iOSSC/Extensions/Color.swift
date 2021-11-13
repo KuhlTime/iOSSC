c383561fbe9614ee61a68ec3e4f98cb9ed7ddbaa
André Kuhlmann <akuhltime@gmail.com>
2021-11-14 00:01:11 +0100

    Add hsd color variable

--- iOSSC/Extensions/Color.swift
+++ iOSSC/Extensions/Color.swift
@@ -20,35 +20,43 @@ extension Color {
     
     // by: https://stackoverflow.com/a/64103125/4179020
     var components: (red: CGFloat, green: CGFloat, blue: CGFloat, opacity: CGFloat) {
-            #if canImport(UIKit)
-            typealias NativeColor = UIColor
-            #elseif canImport(AppKit)
-            typealias NativeColor = NSColor
-            #endif
-
-            var r: CGFloat = 0
-            var g: CGFloat = 0
-            var b: CGFloat = 0
-            var o: CGFloat = 0
-
-            guard NativeColor(self).getRed(&r, green: &g, blue: &b, alpha: &o) else {
-                return (0, 0, 0, 0)
-            }
-            return (r, g, b, o)
-        }
+        #if canImport(UIKit)
+        typealias NativeColor = UIColor
+        #elseif canImport(AppKit)
+        typealias NativeColor = NSColor
+        #endif
         
-        func lighter(by percentage: CGFloat = 30.0) -> Color {
-            return self.adjust(by: abs(percentage) )
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var o: CGFloat = 0
+        
+        guard NativeColor(self).getRed(&r, green: &g, blue: &b, alpha: &o) else {
+            return (0, 0, 0, 0)
         }
-
-        func darker(by percentage: CGFloat = 30.0) -> Color {
-            return self.adjust(by: -1 * abs(percentage) )
-        }
-
-        func adjust(by percentage: CGFloat = 30.0) -> Color {
-            return Color(red: min(Double(self.components.red + percentage/100), 1.0),
-                         green: min(Double(self.components.green + percentage/100), 1.0),
-                         blue: min(Double(self.components.blue + percentage/100), 1.0),
-                         opacity: Double(self.components.opacity))
-        }
+        return (r, g, b, o)
+    }
+    
+    func lighter(by percentage: CGFloat = 30.0) -> Color {
+        return self.adjust(by: abs(percentage) )
+    }
+    
+    func darker(by percentage: CGFloat = 30.0) -> Color {
+        return self.adjust(by: -1 * abs(percentage) )
+    }
+    
+    func adjust(by percentage: CGFloat = 30.0) -> Color {
+        return Color(red: min(Double(self.components.red + percentage/100), 1.0),
+                     green: min(Double(self.components.green + percentage/100), 1.0),
+                     blue: min(Double(self.components.blue + percentage/100), 1.0),
+                     opacity: Double(self.components.opacity))
+    }
+    
+    /**
+     Returns the main color for of the HSD.
+     */
+    static var hsd: Color {
+        return Color(hex: 0xE60028)
+    }
+    
 }
